c841bdf4116b0cdf4375c2a3cd9801e7f43850b9
ram0973 <ram0973@gmail.com>
2024-04-09 16:15:30 +0300

    lesson2

---
+++ src/main/java/dev/simpleapp/twitter/security/model/UserRole.java
@@ -0,0 +1,19 @@
+package dev.simpleapp.twitter.security.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+@Setter
+@Getter
+@Entity
+@Table(schema = "identity", name = "user_roles")
+public class UserRole implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String authority;
+}
